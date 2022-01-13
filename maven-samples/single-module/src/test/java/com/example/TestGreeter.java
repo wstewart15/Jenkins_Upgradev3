358ed7381a8976be544b8fb556881e2686129f09
Will Stewart <gibraltarcub@gmail.com>
2022-01-13 11:31:14 -0500

    Update with Test method

--- maven-samples/single-module/src/test/java/com/example/TestGreeter.java
+++ maven-samples/single-module/src/test/java/com/example/TestGreeter.java
@@ -34,13 +34,14 @@ public class TestGreeter {
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
-    @Test
+  @Test
   public void greetShouldIncludeGreetingMessage() {
     String someone = "World, Thanks you making me Happy";
 
     assertThat(greeter.greet(someone).length(), is(greaterThan(someone.length())));
   }
 
+  @Test
   public void greetShouldIncludeGreetingMessageWill() {
     String someone = "Will, Thanks you making me Happy";
 
